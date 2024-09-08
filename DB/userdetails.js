c74bd72b7fefeb909239561e80daf2c8826e1872
Ashutosh Gangwar <ashutoshgangwar1994@gmail.com>
2024-09-08 17:57:43 +0530

    form schema generated

--- DB/userdetails.js
+++ DB/userdetails.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 
 
 const userSchema = new mongoose.Schema({
-    weightt: Number,
+    weight: Number,
     height: Number,
     bloodgroup: String,
     diet: String,
+    agegroup:Number,
+    gamelevel:String,
+    performance:String,
+    pcoaching:String,
+    coachingplace:String,
+    coachingintrest:String,
     userId:String
 },
 
